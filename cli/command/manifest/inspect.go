4374e06f1e2dc61085e2e4131d98432338b0a152
Christy Perez <christy@linux.vnet.ibm.com>
2017-05-10 16:49:02 -0500

    Add schema1/schema2 manifest output for lists
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/inspect.go
+++ cli/command/manifest/inspect.go
@@ -9,6 +9,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/docker/distribution/manifest/manifestlist"
+	"github.com/docker/distribution/manifest/schema1"
+	"github.com/docker/distribution/manifest/schema2"
 	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/cli"
 	"github.com/docker/docker/cli/command"
@@ -16,8 +18,8 @@ import (
 )
 
 type inspectOptions struct {
-	remote   string
-	platform bool
+	remote  string
+	verbose bool
 }
 
 // NewInspectCommand creates a new `docker manifest inspect` command
@@ -36,7 +38,7 @@ func newInspectCommand(dockerCli *command.DockerCli) *cobra.Command {
 
 	flags := cmd.Flags()
 
-	flags.BoolVarP(&opts.platform, "platform", "p", false, "Output additional info about platform")
+	flags.BoolVarP(&opts.verbose, "verbose", "v", false, "Output additional info including layers and platform")
 
 	return cmd
 }
@@ -73,7 +75,7 @@ func runListInspect(dockerCli *command.DockerCli, opts inspectOptions) error {
 			return err
 		}
 		fmt.Fprintf(dockerCli.Out(), "%s\n", prettyJSON.String())
-		if !opts.platform {
+		if !opts.verbose {
 			return nil
 		}
 		mfd, _, err := buildManifestObj(targetRepo, imgInspect[0])
@@ -115,5 +117,52 @@ func runListInspect(dockerCli *command.DockerCli, opts inspectOptions) error {
 		return err
 	}
 	fmt.Fprintf(dockerCli.Out(), "%s\n", prettyJSON.String())
+	if !opts.verbose {
+		return nil
+	}
+	for _, img := range imgInspect {
+		switch img.MediaType {
+		case schema1.MediaTypeManifest:
+			var manifestv1 schema1.Manifest
+			err := json.Unmarshal(img.CanonicalJSON, &manifestv1)
+			if err != nil {
+				return err
+			}
+			jsonBytes, err := json.Marshal(manifestv1)
+			if err != nil {
+				return err
+			}
+			prettyJSON.Reset()
+			err = json.Indent(&prettyJSON, jsonBytes, "", "    ")
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(dockerCli.Out(), "%s\n", prettyJSON.String())
+		case schema2.MediaTypeManifest:
+			var manifestv2 schema2.Manifest
+			err := json.Unmarshal(img.CanonicalJSON, &manifestv2)
+			if err != nil {
+				return err
+			}
+			jsonBytes, err := json.Marshal(manifestv2)
+			if err != nil {
+				return err
+			}
+			prettyJSON.Reset()
+			err = json.Indent(&prettyJSON, jsonBytes, "", "    ")
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(dockerCli.Out(), "%s\n", prettyJSON.String())
+		}
+		/*
+			prettyJSON.Reset()
+			err = json.Indent(&prettyJSON, jsonBytes, "", "    ")
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(dockerCli.Out(), "%s\n", prettyJSON.String())
+		*/
+	}
 	return nil
 }
